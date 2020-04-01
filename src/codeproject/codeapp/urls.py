60e1003e37cf1846747432986833281e6a2ea43b
Yashas <yashasgowda270@mail.com>
2020-04-01 14:12:37 +0530

    Nothing but a waste commit

--- src/codeproject/codeapp/urls.py
+++ src/codeproject/codeapp/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path,include
 from rest_framework import routers
 from .views import UserViewset,DeveloperViewset,ExperienceViewset,EducationViewset,PostViewset,TokenViewSet
-#from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView,)
 
 router = routers.DefaultRouter()
 router.register('users', UserViewset)
