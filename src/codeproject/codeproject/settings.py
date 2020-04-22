627934a2d7418847611f21a218f6fe0d7a3f9878
Yashas10Gowda <44121735+Yashas10Gowda@users.noreply.github.com>
2020-04-22 21:01:23 +0530

    Update settings.py

--- src/codeproject/codeproject/settings.py
+++ src/codeproject/codeproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(zxmp1&p(4o$=lt+h89rhqdc)m97(80s69)#!tz5ds*7dxe6a6'
+SECRET_KEY = 'N/A'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
